eef754ab855c717147be382db5a259728f299a68
RAbeyrathna <rahal@res-network.com>
2023-12-02 13:57:55 +1100

    Clean up and adjust code into functions. Make code print statements more verbose and clear

--- Day 1/day_1_part2.py
+++ Day 1/day_1_part2.py
@@ -19,8 +19,6 @@ for string in calibration_string_file:
 
 upd_calibration_strings = []
 
-total = 0  # Initializes inital total
-i = 0
 
 # Dictionary to compare when converting string number to integers
 str_list_number = {
@@ -38,58 +36,71 @@ str_list_number = {
 
 int_list_numbers = "0123456789"  # Digits from 1 - 9 in string format to compare within calculation loop
 
+
 # Function to convert string numbers into integers and append to new list
-print("Converting all strings into integer format \n")
-for calibration_string in initial_calibration_strings:
-    print(f"Current string: {calibration_string}")
-    working_str = ""
-    append_str = ""
-    for char in calibration_string:
-        print(f"Current character: {char}")
-        if char in int_list_numbers:
-            append_str = append_str + char
-            print(f"Character is number, appending: {char} \n")
-            continue
-        else:
-            working_str = working_str + char
-            print(
-                f"Character is not a number. Updating working string to {working_str}"
-            )
-            for str_num in str_list_number:
-                if str_num in working_str:
-                    print(
-                        f"Current working string is in list ({working_str}) Appending {str_num} \n"
-                    )
-                    append_str = append_str + str_list_number[str_num]
-                    working_str = working_str[-1]
-                else:
-                    continue
-    upd_calibration_strings.append(append_str)
-print("Completed string conversion. Calculating calibration strings. \n")
+def calstring_to_int():
+    print("Converting all strings into integer format \n")
+    for calibration_string in initial_calibration_strings:
+        print(f"\nSelected string: {calibration_string}")
+        working_str = ""
+        append_str = ""
+        for char in calibration_string:
+            print(f"Current character: {char}")
+            # Compares each character and appends to string if it is an integer
+            if char in int_list_numbers:
+                append_str = append_str + char
+                print(f"Character is number, appending: {char}")
+                continue
+            # if not an integer, check if the characters build into number as string
+            else:
+                working_str = working_str + char
+                print(
+                    f"Character is not a number. Updating working string to {working_str}"
+                )
+                # checks if updated string is in the str_list_number dictionary
+                for str_num in str_list_number:
+                    if str_num in working_str:
+                        print(f"Found and appending string integer {str_num}.")
+                        append_str = append_str + str_list_number[str_num]
+                        # Keep last character of the string in case it is used for the next number
+                        working_str = working_str[-1]
+
+                    else:
+                        continue
+        # Append completed string to new list
+        upd_calibration_strings.append(append_str)
+
+    print("\nCompleted string conversion. \n")
 
 
-for calibration_string in upd_calibration_strings:  # Checks each string provided
-    i += 1
-    print(f"Iteration: {i} - Current calibration string is {calibration_string}")
-    calibration_value = ""  # Initalizes empty string to be appended
-    for char in calibration_string:  # Checks each individual string character
-        if char in int_list_numbers:
-            calibration_value = (
-                calibration_value + char
-            )  # Concatenates the current character if it is present in the list_numbers variable
+# Function to convert calibration strings into calibration values and calculate sum
+def calstring_sum():
+    total = 0  # Initializes inital total
+    i = 0  # Used to track current iteration
+    print("Calculating calibration strings \n")
+    for calibration_string in upd_calibration_strings:  # Checks each string provided
+        i += 1
+        print(f"Iteration: {i} - Current calibration string is {calibration_string}")
+        calibration_value = ""  # Initalizes empty string to be appended
+        for char in calibration_string:  # Checks each individual string character
+            if char in int_list_numbers:
+                calibration_value = (
+                    calibration_value + char
+                )  # Concatenates the current character if it is present in the list_numbers variable
+            else:
+                continue
+        if len(calibration_value) != 2:
+            # Takes only the first and last digits if string is larger than 2 digits
+            first_char = calibration_value[0]
+            last_char = calibration_value[-1]
+            calibration_value = first_char + last_char
+            print(f"Calibration value has been adjusted to {calibration_value}")
         else:
-            continue
-    print(f"Unadjusted calibration value: {calibration_value}")
-    if (
-        len(calibration_value) != 2
-    ):  # Takes only the first and last digits if string is larger than 2 digits
-        first_char = calibration_value[0]
-        last_char = calibration_value[-1]
-        calibration_value = first_char + last_char
-        print("Calibration value has been adjusted")
-    else:
-        print("Current value does not need adjusting. Skipping")
-    print(f"Calibration value is: {calibration_value}")
-    total += int(calibration_value)
-    print(f"Current total is {total} \n")
-print(f"Sum of all calibration values is {total}")
+            print("Current value does not need adjusting. Skipping")
+        total += int(calibration_value)
+        print(f"Current total is {total} \n")
+    print(f"Sum of all calibration values is {total}")
+
+
+calstring_to_int()
+calstring_sum()
